022a51b6b5765d1e9418301be6221b8cbf15ee6f
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-05-31 10:22:05 +0300

    cpp: add main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "native.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    Native native;
+    engine.rootContext()->setContextProperty(QLatin1String("Native"), &native);
+
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
